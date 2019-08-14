1934b49e1b73c4ba018552b2685227523da20dc0
Linden Lu <lindenlu@MacBook-Pro-8.local>
2019-08-14 11:58:39 -0500

    add module2&3

--- lessons/Module3/notebooks/helper_code/mlplots.py
+++ lessons/Module3/notebooks/helper_code/mlplots.py
@@ -7,18 +7,25 @@ import seaborn as sns
 # This method produces a colored heatmap that displays the relationship
 # between predicted and actual types from a machine learning method.
 
-def confusion(test, predict, names, bins=3, title='Confusion Matrix'):
+def confusion(test, predict, labels, title='Confusion Matrix'):
+    '''
+        test: true label of test data, must be one dimensional
+        predict: predicted label of test data, must be one dimensional
+        labels: list of label names, ie: ['positive', 'negative']
+        title: plot title
+    '''
 
+    bins = len(labels)
     # Make a 2D histogram from the test and result arrays
     pts, xe, ye = np.histogram2d(test, predict, bins)
 
     # For simplicity we create a new DataFrame
-    pd_pts = pd.DataFrame(pts.astype(int), index=names, columns=names )
+    pd_pts = pd.DataFrame(pts.astype(int), index=labels, columns=labels )
     
     # Display heatmap and add decorations
     hm = sns.heatmap(pd_pts, annot=True, fmt="d")    
     hm.axes.set_title(title, fontsize=20)
-    hm.axes.set_xlabel('Actual', fontsize=18)
-    hm.axes.set_ylabel('Predicted', fontsize=18)
+    hm.axes.set_xlabel('Predicted', fontsize=18)
+    hm.axes.set_ylabel('Actual', fontsize=18)
 
     return None
